5a3752bd2ef348057c4cafc3ae750a6991f1ab1d
Greg Doud <gpdoud@gmail.com>
2018-10-28 09:53:22 -0400

    disk cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-first-angular-project';
+  title = 'Hello world Java Boot Camp';
+  name = 'Greg';
+  message = 'default message';
 }
